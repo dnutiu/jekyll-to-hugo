5f4566e5989c24e651bc6f1aecad0a9bcb22411f
Denis Nutiu <denis.nutiu@gmail.com>
2023-05-31 19:08:05 +0300

    add tests for header fields drop

--- app/tests/converter/wordpress_markdown_test.py
+++ app/tests/converter/wordpress_markdown_test.py
@@ -25,3 +25,25 @@ def test_fix_hugo_header_rewrite_author(author_rewrite, input_header, expected_h
     )
     converter = WordpressMarkdownConverter(configurator)
     assert converter.fix_hugo_header(input_header) == expected_header
+
+
+@pytest.mark.parametrize(
+    "header_fields_drop, input_header, expected_header",
+    [
+        ([], {}, {"author": ""}),
+        (["a"], {"a": 1, "b": 2, "c": 3}, {"author": "", "b": 2, "c": 3}),
+        (["a", "b"], {"a": 1, "b": 2, "c": 3}, {"author": "", "c": 3}),
+        ([], {"a": 1, "b": 2, "c": 3}, {"author": "", "a": 1, "b": 2, "c": 3}),
+    ],
+)
+def test_fix_hugo_header_fields_drop(header_fields_drop, input_header, expected_header):
+    configurator = make_fake_configurator(
+        "wordpress_markdown_converter",
+        ConverterOptions(
+            author_rewrite="",
+            links_rewrite=[],
+            header_fields_drop=header_fields_drop,
+        ),
+    )
+    converter = WordpressMarkdownConverter(configurator)
+    assert converter.fix_hugo_header(input_header) == expected_header
